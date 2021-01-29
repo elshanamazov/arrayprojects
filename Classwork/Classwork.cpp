3dbfd81592ddc4a1308e2332ea6ba8fedbbf8ec7
Андрей Романов <foxuzer@gmail.com>
2021-01-29 20:48:02 +0400

    task1

--- Classwork/Classwork.cpp
+++ Classwork/Classwork.cpp
@@ -5,7 +5,27 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    
 
+    const int size = 4;
+    
+    int arr[size];
+
+    int n = 0;
+    int sum = 0;
+
+    cout << "Введите значение \n";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> n;
+        arr[i] = n;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        sum += arr[i];
+        cout << sum << "|";
+    }
+
+    return 0;
 }
 
